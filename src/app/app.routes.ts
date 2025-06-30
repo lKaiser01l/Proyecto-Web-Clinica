b8cac2f4ec06c4e57daa67172e24060cc19a1a93
Kaiser <arianocuellar03@outlook.com>
2025-06-29 20:36:52 -0500

    arreglo de errores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,14 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HomeComponent }      from './pages/home/home.component';
-import { LabComponent }       from './pages/lab/lab.component';
-import { DoctoresComponent }  from './pages/doctores/doctores.component';
-import { BlogSaludComponent } from './pages/blog-salud/blog-salud.component'; 
-import { ServiciosComponent } from './pages/servicios/servicios.component';
-
-export const routes: Routes = [
-  { path: '',         component: HomeComponent, pathMatch: 'full' },
-  { path: 'lab',      component: LabComponent },     // minúsculas
-  { path: 'doctores', component: DoctoresComponent },
-  { path: 'blog',       component: BlogSaludComponent },
-    { path: 'servicios', component: ServiciosComponent },
-  { path: '**',       redirectTo: '' }               // 404 simple
-=======
 
 
 import { LabComponent } from './pages/lab/lab.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DoctoresComponent }  from './pages/doctores/doctores.component';
 import { LoginPacienteComponent } from './pages/autenticacion/login-paciente/login-paciente.component';
 import { CitaComponent } from './pages/compartido/cita/cita.component';
 import { FooterComponent } from './pages/compartido/footer/footer.component';
+import { BlogSaludComponent } from './pages/blog-salud/blog-salud.component'; 
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
 export const routes: Routes = [
     {
@@ -38,7 +26,19 @@ export const routes: Routes = [
     },{
         path: 'footer',
         component: FooterComponent
-    }
+    },{ 
+        path: 'doctores',
+         component: DoctoresComponent 
+    },{ 
+        path: 'blog',
+        component: BlogSaludComponent 
+    },{ 
+        path: 'servicios',
+        component: ServiciosComponent 
+    },{
+        path: '**',
+        redirectTo: '' 
+    } 
     
->>>>>>> b682b284d1af8efcc383ad79ce50e6b6346f6d13
+
 ];
